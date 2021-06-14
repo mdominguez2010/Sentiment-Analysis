110f41387c70e87a7b9bc65d63e30ac782a82d7a
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-06-14 11:44:03 -0700

    initial commit

---
+++ Twitter/Streaming-Live-Tweets/credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter API
+ACCESS_TOKEN = '284664026-vnKq8vYCJEHeFGh47YzVMKQXJx0TODoVCOuo2Vuj'
+ACCESS_TOKEN_SECRET = 'ahyQjYgiM43yEGsPnmGlxIzUJgTPFIVAisH6qH4v8Gvay'
+CONSUMER_KEY = 'vJsHWjSbt1LjZ0dCTc0ILmtN8' # api_key
+CONSUMER_SECRET = 'TawM1d88l73nRHTdhKEuPQCWcxdd8Pl9nWYsFFqj5XnvjEl1k3' # api_key_secret
